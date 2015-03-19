6e23ee83ccbefcc78c3d5b64bb0618430be16ce3
KimJeongHwan <kjq3052@naver.com>
2015-03-19 21:30:31 +0900

    get Android application API key

---
+++ app/src/main/java/org/androidtown/citytour/MainActivity3_jukbyuk.java
@@ -0,0 +1,14 @@
+package org.androidtown.citytour;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class MainActivity3_jukbyuk extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3_jukbyuk);
+    }
+}
